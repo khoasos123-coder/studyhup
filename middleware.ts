01da7fcd3d65a026d53903fe90ae8aa7b238a87c
D3aN-CT <khoasos123@gmail.com>
2026-09-21 15:08:22 +0700

    fix: update middleware for next-auth v5

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,4 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  function middleware(req) {
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-    pages: {
-      signIn: "/login",
-    },
-  }
-);
+export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: [
